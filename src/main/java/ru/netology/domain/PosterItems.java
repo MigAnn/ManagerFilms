c0e46decf5c3f0a3310b30004a2c81af6d9a0768
Анна <anakatkova166@yandex.ru>
2022-06-08 13:40:11 +0300

    PosterManager

---
+++ src/main/java/ru/netology/domain/PosterItems.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class PosterItems {
+    private int id;
+    private String filmName;
+    private String genre;
+
+}
---
+++ src/main/java/ru/netology/manager/test/PosterManagerTest.java
@@ -0,0 +1,67 @@
+package ru.netology.manager.test;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import ru.netology.domain.PosterItems;
+import ru.netology.manager.PosterManager;
+
+public class PosterManagerTest {
+
+    PosterItems first = new PosterItems(404, "Bloodshot", "action movie");
+    PosterItems second = new PosterItems(902, "Forward", "cartoon");
+    PosterItems third = new PosterItems(311, "Hotel Belgrade", "comedy");
+    PosterItems fourth = new PosterItems(26, "Gentlemen", "action movie");
+    PosterItems fifth = new PosterItems(298, "The Invisible Man", " horror");
+    PosterItems sixth = new PosterItems(756, "Number one", " comedy");
+    PosterItems seventh = new PosterItems(310, "Coati", "cartoon ");
+    PosterItems eighth = new PosterItems(418, "Amanat", "history ");
+    PosterItems ninth = new PosterItems(745, "21 bridges", "action movie");
+    PosterItems tenth = new PosterItems(278, "Gauguin", "documentary ");
+
+    @Test
+    public void findAll() {
+        PosterManager manager = new PosterManager();
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+        manager.add(eighth);
+        manager.add(ninth);
+        manager.add(tenth);
+
+        manager.findAll();
+        manager.findLast();
+
+
+        PosterItems[] actual = manager.findLast();
+        PosterItems[] expected = {tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void findLast() {
+        PosterManager manager = new PosterManager();
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+        manager.add(eighth);
+        manager.add(ninth);
+        manager.add(tenth);
+
+        manager.findAll();
+
+        PosterItems[] actual = manager.findAll();
+        PosterItems[] expected = {first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth};
+
+        Assertions.assertArrayEquals(actual,expected);
+
+    }
+}
